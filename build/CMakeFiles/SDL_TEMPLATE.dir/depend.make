71595efd67c17b9335f2c6522e6646fac2bb3c87
AlexTriedLife <alexmcphersonuribe@gmail.com>
2025-03-20 17:35:07 -0400

    Base

---
+++ build/CMakeFiles/SDL_TEMPLATE.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for SDL_TEMPLATE.
+# This may be replaced when dependencies are built.
---
+++ src/main.cpp
@@ -0,0 +1,50 @@
+#include <SDL3/SDL.h>
+#include <iostream>
+
+int main(int argc, char *argv[]) {
+  if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+    std::cerr << "SDL could not initialize! SDL_Error: " << SDL_GetError()
+              << std::endl;
+    return 1;
+  }
+
+  SDL_Window *window =
+      SDL_CreateWindow("SDL3 Template", 800, 600, SDL_WINDOW_RESIZABLE);
+  if (!window) {
+    std::cerr << "Window could not be created! SDL_Error: " << SDL_GetError()
+              << std::endl;
+    SDL_Quit();
+    return 1;
+  }
+
+  SDL_Renderer *renderer =
+      SDL_CreateRenderer(window, nullptr);
+  if (!renderer) {
+    std::cerr << "Renderer could not be created! SDL_Error: " << SDL_GetError()
+              << std::endl;
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    return 1;
+  }
+
+  bool running = true;
+  SDL_Event event;
+
+  while (running) {
+    while (SDL_PollEvent(&event)) {
+      if (event.type == SDL_EVENT_QUIT) {
+        running = false;
+      }
+    }
+
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+    SDL_RenderClear(renderer);
+    SDL_RenderPresent(renderer);
+  }
+
+  SDL_DestroyRenderer(renderer);
+  SDL_DestroyWindow(window);
+  SDL_Quit();
+
+  return 0;
+}
